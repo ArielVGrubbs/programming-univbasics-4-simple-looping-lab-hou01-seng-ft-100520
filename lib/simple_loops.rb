3cad29a6937ae1da6b294e5568e8bac1308812bf
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-06 01:30:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,3 +14,11 @@ def loop_message_n_times (message, number)
     counter += 1
   end
 end
+
+def output_array (message, number)
+  counter = 0
+  while counter < number do
+    puts message
+    counter += 1
+  end
+end
